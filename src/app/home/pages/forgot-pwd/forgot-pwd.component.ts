58237575f00a5cf8dde93c8bc807e503e0bc285d
henryalejandro1229 <101129308+henryalejandro1229@users.noreply.github.com>
2023-04-02 13:56:13 -0600

    Fix - forms

--- src/app/home/pages/forgot-pwd/forgot-pwd.component.ts
+++ src/app/home/pages/forgot-pwd/forgot-pwd.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-pwd',
   templateUrl: './forgot-pwd.component.html',
-  styleUrls: ['./forgot-pwd.component.scss']
+  styleUrls: ['./forgot-pwd.component.scss'],
 })
 export class ForgotPwdComponent implements OnInit {
+  form!: FormGroup;
+  clear: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required]),
+    });
   }
 
+  ngOnInit(): void {}
+
+  forgotPwd() {
+
+  }
 }
