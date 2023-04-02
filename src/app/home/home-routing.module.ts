afd81404ccab432c84fa21210eed878186f50f75
henryalejandro1229 <101129308+henryalejandro1229@users.noreply.github.com>
2023-04-02 13:37:37 -0600

    Feat - form

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { LoginComponent } from './pages/login/login.component';
+import { SingUpComponent } from './pages/sing-up/sing-up.component';
+import { ForgotPwdComponent } from './pages/forgot-pwd/forgot-pwd.component';
+import { RegisterProcessComponent } from './pages/register-process/register-process.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
 
 const routes: Routes = [
   {
@@ -15,15 +19,35 @@ const routes: Routes = [
       },
       {
         path: 'login',
-        data: { breadcrumb: 'login'},
+        data: { breadcrumb: 'Iniciar sesión' },
         component: LoginComponent,
       },
+      {
+        path: 'singup',
+        data: { breadcrumb: 'Registrate' },
+        component: SingUpComponent,
+      },
+      {
+        path: 'forgot-pwd',
+        data: { breadcrumb: 'Recuperar contraseña' },
+        component: ForgotPwdComponent,
+      },
+      {
+        path: 'singup-process',
+        data: { breadcrumb: 'Completa tu información' },
+        component: RegisterProcessComponent,
+      },
+      {
+        path: 'reset-password',
+        data: { breadcrumb: 'Actualizar contraseña' },
+        component: ResetPasswordComponent,
+      },
     ],
-  }
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
