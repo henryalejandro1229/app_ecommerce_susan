169783c2f5e96cd7fafab99ee8f7623bafa19907
Henry Alejandro <henryalejandro1229@gmail.com>
2023-04-11 11:40:29 -0600

    Feat - page no found

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _route : Router, private _auth: AuthService) { }
 
   ngOnInit(): void {
+    if (this._auth.isAdmin) {
+      this._route.navigate(['/admin'])
+    }
   }
 
 }
--- src/app/shared/page-no-found/page-no-found.component.ts
+++ src/app/shared/page-no-found/page-no-found.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-no-found',
   templateUrl: './page-no-found.component.html',
-  styleUrls: ['./page-no-found.component.scss']
+  styleUrls: ['./page-no-found.component.scss'],
 })
 export class PageNoFoundComponent implements OnInit {
+  constructor(public _auth: AuthService, private _router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  routerLink() {
+    if (this._auth.isAdmin) {
+      this._router.navigate(['/admin']);
+      return;
+    }
+    this._router.navigate(['/home']);
   }
-
 }
