a86bbb29dfd3b5353424e5ceda5be837737166eb
Henry Alejandro <henryalejandro1229@gmail.com>
2023-08-20 18:48:06 -0600

    Feat - Firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,4 +4,12 @@ export const environment = {
   urlCOPOMEX: `https://api.copomex.com/query/`,
   urlImg: `https://licordesabor.proyectowebuni.com/api/products/imageProducts/`,
   urlEmail: `https://appsaborlicorserveremail-production.up.railway.app/saborlicor_app_backend/`,
+  firebaseConfig: {
+    projectId: 'licores-de-sabor',
+    appId: '1:103220365677:web:52f63c3fa2c6d7385db264',
+    storageBucket: 'licores-de-sabor.appspot.com',
+    apiKey: 'AIzaSyC-RaNBY6YIZ7mwF3aGwf7nSLPDm7YFJVU',
+    authDomain: 'licores-de-sabor.firebaseapp.com',
+    messagingSenderId: '1022333183004',
+  }
 };
